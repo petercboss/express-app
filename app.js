18289422b7c64f123f74eb1b8a80da33f76df8b9
Peter Boss <m@Peters-MacBook-Pro.local>
2018-02-22 15:02:21 -0600

    with redirect

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.route('/').get((req, res) => res.send('Hello World!'))
 .post((req, res) => res.send('Got a POST request'))
-.put((req, res) => res.send('Got a PUT request at /user'));
+.put((req, res) => res.send('Got a PUT request at /'));
+
+app.get('/redirect', (req, res) => res.redirect(301, '/'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
